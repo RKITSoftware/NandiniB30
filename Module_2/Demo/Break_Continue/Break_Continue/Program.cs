632d0e2c0fd253749d36a2cd7d88e5ffb0dbec39
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-10-13 11:49:21 +0530

    Add files via upload

---
+++ Module_2/Demo/Break_Continue/Break_Continue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Break_Continue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Break 
+            //Loop breaks when i Becomes 2
+            for (int i = 0; i < 4; i++)
+            {
+                //check if i is 2
+                if (i == 2)
+                    //if true, break out of loop
+                    break;
+
+                Console.WriteLine("i = " + i);
+            }
+            Console.ReadLine();
+            #endregion Break
+
+            #region Continue
+            // if the value of i becomes even then it will skip to next iteration, i.e, i+1 
+            for (int i = 1; i <= 6; i++)
+            {
+                //check if i is even
+                if (i %2 == 0)
+                {
+                    //if true, then continue to next iteration
+                    continue;
+                }
+                Console.WriteLine("i = " + i);
+            }
+            Console.ReadLine();
+            #endregion Continue
+
+        }
+    }
+}
