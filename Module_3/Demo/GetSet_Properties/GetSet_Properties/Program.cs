c7c02f590a5731b99bc7a6f761b3b603f3cb0095
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-10-29 22:16:56 +0530

    Add files via upload

---
+++ Module_3/Demo/GetSet_Properties/GetSet_Properties/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GetSet_Properties
+{
+    class Personal_Information
+    {
+        //properties Getter and setter is used to set or get value.
+        public static string name { get; set; }
+        public static int age { get; set; }
+    }
+    class Personal_Information_demo
+    {
+        static void Main(string[] args)
+        {
+            //set the value.
+            Personal_Information.name = "Nandini Bhattacharya";
+            Personal_Information.age = 21;
+
+            //get the value.
+            Console.WriteLine("Name : " + Personal_Information.name);
+            Console.WriteLine("Age : " + Personal_Information.age);
+            Console.ReadLine();
+        }
+    }
+}
