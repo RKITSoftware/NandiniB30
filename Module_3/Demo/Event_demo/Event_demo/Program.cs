0a3cd593b236b7bd047da1aa0842dc9ac8c9c3fc
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-10-29 22:03:43 +0530

    Add files via upload

---
+++ Module_3/Demo/Event_demo/Event_demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_demo
+{
+    //This is Subscriber Class
+    class Numbers
+    {
+        static void Main(string[] args)
+        {
+            AddTwoNumbers objNum = new AddTwoNumbers();
+            //Event gets binded with delegates
+            objNum.ev_OddNumber += new AddTwoNumbers.dg_OddNumber(EventMessage);
+            objNum.Add();
+            Console.Read();
+        }
+        //Delegates calls this method when event raised.  
+        static void EventMessage()
+        {
+            Console.WriteLine("Event Executed : This is Odd Number");
+        }
+    }
+    //This is Publisher Class
+    class AddTwoNumbers
+    {
+        //Declared Delegate
+        public delegate void dg_OddNumber();
+        //Declared Events
+        public event dg_OddNumber ev_OddNumber; 
+
+        public void Add()
+        {
+            int result;
+            result = 5 + 4;
+            Console.WriteLine(result.ToString());
+            //Check if result is odd number then raise event
+            if ((result % 2 != 0) && (ev_OddNumber != null))
+            {
+                ev_OddNumber(); //Raised Event
+            }
+        }
+    }
+}
