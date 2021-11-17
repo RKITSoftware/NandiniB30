f41f40e0e0eda6f2daf5327026f628708c1ef8e5
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-11-17 20:00:32 +0530

    Add files via upload

---
+++ Module_4/DEMO/enumeration/enumeration/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace enumeration
+{
+    //declare an enum 
+    public enum colour
+    {
+        Red,
+        Green=2,
+        Yellow,
+        Blue=7,
+        Orange
+    }
+
+    class enumeration_demo
+    {
+        static void Main(string[] args)
+        {
+            //explicit type conversion from enum to int
+            int var_yellow = (int)colour.Yellow;
+            int var_blue = (int)colour.Blue;
+
+            //Value of colour yellow : 3
+            Console.WriteLine("Value of colour yellow : {0} ", var_yellow);
+
+            // Value of colour blue : 7
+            Console.WriteLine("Value of colour blue : {0} ", var_blue);
+
+            //conversion of an int value to enum value
+            int colour_value = 2;
+            colour find_colour = (colour)colour_value;
+
+            // The colour is Green
+            Console.WriteLine("The colour is {0}", find_colour);
+
+            //get the integer value of the enum members
+            Array enumColourValue = Enum.GetValues(typeof(colour));
+            foreach (int enumValue in enumColourValue)
+            {
+                // get the value/name of the enum members
+                string colorValue = Enum.GetName(typeof(colour), enumValue);
+                Console.WriteLine($"Name: {colorValue} and value : {enumValue}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
