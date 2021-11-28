9e041101ad0c776ecd17135ba43e3a44ee3e91cc
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-11-28 21:05:52 +0530

    Add files via upload

---
+++ Module_5/DEMO/Serialization/JSON_Serialization/JSON_Serialization/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JSON_Serialization.Controllers
+{
+    [ApiController]
+    //routing done for all methods in controller
+    [Route("[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public List<Employee> Get()
+        {
+            //create an object
+            List<Employee> obj_empList = new List<Employee>();
+            
+            //add new data to the list
+            obj_empList.Add(new Employee()
+            {
+                EmployeeID = 1,
+                FirstName = "Nandini",
+                LastName = "Bhattacharya"
+            });
+
+            //add new data to the list
+            obj_empList.Add(new Employee()
+            {
+                EmployeeID = 2,
+                FirstName = "Diti",
+                LastName = "Roy"
+            });
+
+            return obj_empList;
+        }
+    }
+}
