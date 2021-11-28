37a307ea43a1148af1a391c573ac1a7d4173c863
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-11-28 21:16:29 +0530

    Add files via upload

---
+++ Module_5/DEMO/Serialization/Binary_XML_Serialization/Binary_Serialization/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Serialize_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee {
+                name = textBoxName.Text,
+                phone = textBoxPhone.Text,
+                dob = dateTimePickerDoB.Value,
+                department = textBoxDepartment.Text,
+                salary = Convert.ToInt32(textBoxSalary.Text),
+                additionalInfo = "We don't want it to serialize"
+            };
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream fsout = new FileStream("employee.binary", FileMode.Create, FileAccess.Write, FileShare.None);
+            try
+            {
+                using (fsout)
+                {
+                    bf.Serialize(fsout, emp);
+                    label6.Text = "Object Serialized";
+                }
+            }
+            catch
+            {
+                label6.Text = "An error has occured";
+            }
+        }
+
+        private void Deserialize_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream fsin = new FileStream("employee.binary", FileMode.Open, FileAccess.Read, FileShare.None);
+            try
+            {
+                using (fsin)
+                {
+                    emp = (Employee) bf.Deserialize(fsin);
+                    label6.Text = "Object Deserialized";
+
+                    textBoxName.Text = emp.name;
+                    textBoxPhone.Text = emp.phone;
+                    dateTimePickerDoB.Value = emp.dob;
+                    textBoxDepartment.Text = emp.department;
+                    textBoxSalary.Text = emp.salary.ToString();
+                }
+            }
+            catch
+            {
+                label6.Text = "An error has occured";
+            }
+        }
+    }
+}
