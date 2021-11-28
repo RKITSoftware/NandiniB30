04ca91b63515e73ebdc8fb2b68d1314cede7e836
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-11-28 21:08:05 +0530

    Add files via upload

---
+++ Module_5/DEMO/Serialization/JSON_Serialization/JSON_Serialization/Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace JSON_Serialization
+{
+    public class Employee
+    {
+        //name the properties for the list
+        public int EmployeeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
