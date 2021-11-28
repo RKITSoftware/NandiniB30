475ce3cf0ec09dfcaef23adb00b192907df69c27
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2021-11-28 20:47:35 +0530

    Add files via upload

---
+++ Module_5/DEMO/Controllers/StudentDataController.cs
@@ -0,0 +1,24 @@
+﻿using MyFirstMVCApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class StudentDataController : Controller
+    {
+        // GET: StudentData
+        //http:/url/StudentData/ShowTheStudentDetail 
+        public ActionResult ShowTheStudentDetail()
+        {
+            StudentData obj_StudentData = new StudentData();
+            //create list with various student data
+            obj_StudentData.CreateListOfStudents();
+            
+            //pass the content to view
+            return View(obj_StudentData );
+        }
+    }
+}
