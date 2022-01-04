a46b604727d476a3bd3d60aca99a073b67b97dd7
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2022-01-04 20:22:53 +0530

    Update ValuesController.cs

--- Module_6/DEMO/Implementing_HTTP_Verbs/Controllers/ValuesController.cs
+++ Module_6/DEMO/Implementing_HTTP_Verbs/Controllers/ValuesController.cs
@@ -12,38 +12,37 @@ namespace Implementing_HTTP_Verbs.Controllers
             "Wheel of Time","The Fault of our Stars","Golden Couple"
         };
 
-        // GET api/values
         //This gives result to GET method without parameter
         [Route("")]
+        [HttpGet]
         public IEnumerable<string> Get()
         {
             return obj_books;
         }
 
-        // GET api/values/5
         [Route("{int:id}")]
+        [HttpGet]
         public string Get(int id)
         {
             return obj_books[id];
         }
 
-        // POST api/values
         //used to create new item
         [Route("")]
+        [HttpPost]
         public void Post([FromBody] string value)
         {
             obj_books.Add(value);
         }
 
-        // PUT api/values/5
         //used to update an item: "id" is what we update with "value"
         [Route("int:id")]
+        [HttpPut]
         public void Put(int id, [FromBody] string value)
         {
             obj_books[id] = value;
         }
 
-        // DELETE api/values/5
         //used to remove an item
         [Route("int:id")]
         [HttpDelete]
