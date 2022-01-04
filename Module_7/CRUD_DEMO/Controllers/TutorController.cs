ff100f1d0669c7b262c3ca537216f3ecf0bc3a2e
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2022-01-04 15:58:22 +0530

    Add files via upload

---
+++ Module_7/CRUD_DEMO/Controllers/TutorController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CRUD_Demo.Models;
+
+namespace CRUD_Demo.Controllers
+{
+    public class TutorController : ApiController
+    {
+        database_DB objdatabase = new database_DB();
+
+        // GET: api/Book
+        [Route("api/Getall")]
+        public List<Tutor> GetAll()
+        {
+            return (objdatabase.selectAll());
+        }
+
+        // GET: api/Book/5
+        [Route("api/Get/{id}")]
+        public Tutor Get(int id)
+        {
+            return (objdatabase.getTutorById(id));
+        }
+
+        // POST: api/Book
+        [HttpPost]
+        [Route("api/Add")]
+        public void Add([FromBody]Tutor tutoring)
+        {
+            objdatabase.addTutor(tutoring);
+        }
+
+        [HttpPost]
+        [Route("api/Update")]
+        public void Update([FromBody] Tutor tutoring)
+        {
+            objdatabase.updateTutor(tutoring);
+        }
+
+        // DELETE: api/Book/5
+        [Route("api/Delete/{id}")]
+        public void Delete(int id)
+        {
+            objdatabase.deleteTutor(id);
+        }
+    }
+}
---
+++ Module_7/CRUD_DEMO/Models/Tutor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_Demo.Models
+{
+    public class Tutor
+    {
+        [Key]
+        public int TutorID { get; set; }
+        public string TutorName { get; set; }
+        public string Subject_to_Teach { get; set; }
+    }
+}
