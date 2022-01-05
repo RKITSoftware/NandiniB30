b4ffec6167645b4ef1763cdf865a157415a816ad
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2022-01-05 10:31:46 +0530

    Add files via upload

---
+++ Module_5/DEMO/Serialization/SerializationJSON/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SerializationJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //populate the class with tutor data using an object
+            Tutor obj_tutor = new Tutor()
+            {
+                TutorID = 1,
+                TutorName = "Nandini",
+                TutorEmail="nb@gmail.com",
+                Subject_to_Teach="English"
+            };
+
+            // Convert Tutor object to JOSN string format using JsonConvert.SerializeObject method
+            string jsonData = JsonConvert.SerializeObject(obj_tutor);
+            Console.WriteLine(jsonData);
+
+            string json = @"{
+                              'TutorName': 'Nandini',
+                              'TutorID': 1,
+                              'TutorEmail':'nb@gmail.com',
+                              'Subject to Teach':'English'
+                            }";
+
+            //deserialize JSON Object using JsonObject.DeserializeObject
+            Tutor Obj_deserialized = JsonConvert.DeserializeObject<Tutor>(json);
+            Console.Write(Obj_deserialized.TutorName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Module_5/DEMO/Serialization/SerializationJSON/Tutor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SerializationJSON
+{
+    /// <summary>
+    /// Class Tutor contains four attributes that are to be serialized.
+    /// </summary>
+    [Serializable]
+    class Tutor
+    {
+        public int TutorID { get; set; }
+        public string TutorName { get; set; }
+        public string TutorEmail { get; set; }
+        public string Subject_to_Teach { get; set; }
+    }
+}
