0770acf34299c691a9b1d3c3a4f8f45d74f4c3fe
nandini_b <71935037+NandiniB30@users.noreply.github.com>
2022-01-05 11:42:18 +0530

    Add files via upload

---
+++ Module_7/CRUD_DEMO/Controllers/TutorController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using CRUD_Demo.Models;
+
+namespace CRUD_Demo.Controllers
+{
+    public class TutorController : ApiController
+    {
+        BLTutor objdatabase = new BLTutor();
+
+        /// <summary>
+        /// Get all tutor data
+        /// </summary>
+        /// <returns>
+        /// returns a tutor data
+        /// </returns>
+        [HttpGet]
+        [Route("api/Getall")]
+        public List<Tutor> GetAll()
+        {
+            return (objdatabase.selectAll());
+        }
+
+        /// <summary>
+        /// Get tutor data of particular parameter 'id'
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>
+        /// returns tutor data according to the ID
+        /// </returns>
+        [HttpGet]
+        [Route("api/Get/{id}")]
+        public Tutor Get(int id)
+        {
+            return (objdatabase.getTutorById(id));
+        }
+
+        /// <summary>
+        /// Add new tutor data
+        /// </summary>
+        /// <param name="tutoring">
+        /// tutoring is the tutor object.
+        /// </param>
+        [HttpPost]
+        [Route("api/Add")]
+        public void Add([FromBody]Tutor tutoring)
+        {
+            objdatabase.addTutor(tutoring);
+        }
+
+
+        /// <summary>
+        /// Update tutor data
+        /// </summary>
+        /// <param name="tutoring">
+        /// tutoring is the tutor object.
+        /// </param>
+        [HttpPost]
+        [Route("api/Update")]
+        public void Update([FromBody] Tutor tutoring)
+        {
+            objdatabase.updateTutor(tutoring);
+        }
+
+        /// <summary>
+        /// Delete tutor data with parameter 'id'
+        /// </summary>
+        [HttpDelete]
+        [Route("api/Delete/{id}")]
+        public void Delete(int id)
+        {
+            objdatabase.deleteTutor(id);
+        }
+    }
+}
